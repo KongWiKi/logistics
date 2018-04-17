0bc9276354ba91858d430e36337c4935a32abb3c
KongWiKi <kongwiki@163.com>
2018-04-17 22:37:13 +0800

    添加页面

--- app/main/views.py
+++ app/main/views.py
@@ -5,10 +5,31 @@
 @contact: kongwiki@163.com
 '''
 from flask import render_template
-
 from  . import main
+
+import pymysql
+host='localhost'
+user='root'
+pwd='Hanhuan.0214'
+database='webLog'
+con=pymysql.connect(host,user,pwd,database)
+cursor=con.cursor()
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/chart')
+def chart():
+    return render_template('chart.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+# @main.route('/table')
+# def datatable():
+#     cursor.execute('select * from log where value>5')
+#     return render_template('datatable.html', data=cursor.fetchall())

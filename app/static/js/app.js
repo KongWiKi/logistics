a4c31f3a658efb952f0c137198f3bb51c7f42d0a
KongWiKi <kongwiki@163.com>
2018-04-23 09:53:05 +0800

    添加综合评判图表

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -433,7 +433,6 @@ var pageData = {
         //第三张图
         // 车速展示
         var echartsB = echarts.init(document.getElementById('tpl-echarts-B'));
-
         optionB = {
     backgroundColor: '#1b1b1b',
     tooltip : {
@@ -719,7 +718,6 @@ var pageData = {
         optionB.series[3].data[0].value = (Math.random()*2).toFixed(2) - 0;
         echartsB.setOption(optionB);
         },2000);
-        // echartsB.setOption(optionB);
 
         //第四张图
         //水球 打卡
@@ -993,6 +991,105 @@ var pageData = {
     }]
            })
        });
+
+    },
+
+    // ===============================================
+    // 综合分析页面图表
+    // ===============================================
+
+    'general':function generalData(){
+        echartsRadarDriver = echarts.init(document.getElementById('tpl-echarts-Radar-Driver'));
+        optionRadarDriver = {
+    title: {
+        text: '基础雷达图'
+    },
+    tooltip: {},
+    legend: {
+        data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+    },
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+           }
+        },
+        indicator: [
+           { name: '销售（sales）', max: 6500},
+           { name: '管理（Administration）', max: 16000},
+           { name: '信息技术（Information Techology）', max: 30000},
+           { name: '客服（Customer Support）', max: 38000},
+           { name: '研发（Development）', max: 52000},
+           { name: '市场（Marketing）', max: 25000}
+        ]
+    },
+    series: [{
+        name: '预算 vs 开销（Budget vs spending）',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data : [
+            {
+                value : [4300, 10000, 28000, 35000, 50000, 19000],
+                name : '预算分配（Allocated Budget）'
+            },
+             {
+                value : [5000, 14000, 28000, 31000, 42000, 21000],
+                name : '实际开销（Actual Spending）'
+            }
+        ]
+    }]
+};
+        echartsRadarDriver.setOption(optionRadarDriver);
+
+        echartsRadarTeam = echarts.init(document.getElementById('tpl-echarts-Radar-Team'));
+        optionRadarTeam = {
+    title: {
+        text: '基础雷达图'
+    },
+    tooltip: {},
+    legend: {
+        data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+    },
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+           }
+        },
+        indicator: [
+           { name: '销售（sales）', max: 6500},
+           { name: '管理（Administration）', max: 16000},
+           { name: '信息技术（Information Techology）', max: 30000},
+           { name: '客服（Customer Support）', max: 38000},
+           { name: '研发（Development）', max: 52000},
+           { name: '市场（Marketing）', max: 25000}
+        ]
+    },
+    series: [{
+        name: '预算 vs 开销（Budget vs spending）',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data : [
+            {
+                value : [4300, 10000, 28000, 35000, 50000, 19000],
+                name : '预算分配（Allocated Budget）'
+            },
+             {
+                value : [5000, 14000, 28000, 31000, 42000, 21000],
+                name : '实际开销（Actual Spending）'
+            }
+        ]
+    }]
+};
+        echartsRadarTeam.setOption(optionRadarTeam)
 
     },
 

4e882254d126df1be9bd9bfd1f477e4de0bce0a0
KongWiki <kongwiki@163.com>
2018-12-26 16:26:56 +0800

    delete

--- app/email.py
+++
@@ -1,6 +0,0 @@
-'''
-@author：KongWeiKun
-@file: email.py
-@time: 18-4-17 下午2:19
-@contact: kongwiki@163.com
-'''
--- app/main/error.py
+++
@@ -1,6 +0,0 @@
-'''
-@author：KongWeiKun
-@file: error.py
-@time: 18-4-17 下午2:18
-@contact: kongwiki@163.com
-'''
